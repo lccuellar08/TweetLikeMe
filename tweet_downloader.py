dbd622fb7426fe4f54402703b53342a76ebdad69
lccuellar08 <lc2377@us.att.com>
2020-01-17 20:57:45 -0600

    API Key pointing to config file now

--- tweet_downloader.py
+++ tweet_downloader.py
@@ -4,12 +4,13 @@
 import tweepy #https://github.com/tweepy/tweepy
 import csv
 import sys
+import config as cfg
 
 #Twitter API credentials
-consumer_key = "KgqgwGMo9JAVZBKjw4V2I4sqL"
-consumer_secret = "cJkVuJmznG1akyeRKegzm5VNInFeRJ1n5a9ZKX2V2jYg444VUT"
-access_key = "913578326-foEZmij61QRVsS0DhKSGFLkA9NYL6fmAa7ElRvJc"
-access_secret = "BqQv2b5Ef88csR6D3nflN0j5lkzoTt2mZMGozSynPwmUM"
+consumer_key = cfg.consumer_key
+consumer_secret = cfg.consumer_secret
+access_key = cfg.access_key
+access_secret = cfg.access_secret
 
 
 def get_all_tweets(screen_name):
