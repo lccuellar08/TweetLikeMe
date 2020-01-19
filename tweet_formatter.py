2d259f4ffc22044a5d833772ef3251f93530a0c2
lccuellar08 <lc2377@us.att.com>
2020-01-18 18:21:58 -0600

    Cleaned up code and documented

--- tweet_formatter.py
+++ tweet_formatter.py
@@ -8,15 +8,25 @@ import sys
 
 def main(screenname):
 	df = pd.read_csv(screenname+"_tweets.csv")
+
+	# Remove all text that contains "http" or ""
 	for i,row in df.iterrows():
 		tokens = row['text'].split(" ")
 		tokens = [s for s in tokens if "http" not in s and "@" not in s]
 		df.at[i,'text_f'] = " ".join(str(s) for s in tokens)
+
+	# Format all text as lowercase
 	df['text_f'] = df['text_f'].apply(lambda x: x.lower())
+
+	# Remove all non alphabet characters
 	df['text_f'] = df['text_f'].apply((lambda x: re.sub('[^a-zA-z0-9\s]','',x)))
+
+	# Remove all retweets
 	df = df[~df['text_f'].str.contains("rt")]
+
+	# Remove all empty tweets
 	df = df[df['text_f'] != '']
-	print(df['text_f'])
+
 	df.to_csv(screenname+"_formatted.csv", index=False)
 
 if __name__ == '__main__':
